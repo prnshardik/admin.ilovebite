bad54157d50db8d646c3d0d9a966377b01233a54
HardIk Patel <prns.hardik@gmail.com>
2021-03-15 20:55:45 +0530

    init

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', 'AuthController@login');
+Route::get('/', 'AuthController@login');
 
-Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 Route::get('/page', 'HomeController@page');
